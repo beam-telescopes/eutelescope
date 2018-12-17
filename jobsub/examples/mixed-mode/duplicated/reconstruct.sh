396a448300bf99d8a0745590e0aa89be3b9f0efb
Cyril Becot <cyril.becot@cern.ch>
2018-12-17 17:03:41 +0100

    Some fixes

--- jobsub/examples/mixed-mode/duplicated/reconstruct.sh
+++ jobsub/examples/mixed-mode/duplicated/reconstruct.sh
@@ -1,9 +1,9 @@
-#jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 497
-#jobsub -c config.cfg -csv runlist.csv -g clustering 497
-#jobsub -c config.cfg -csv runlist.csv -g hitmaker 497
-#jobsub -c config.cfg -csv runlist.csv -g alignGBL1 497
-#jobsub -c config.cfg -csv runlist.csv -g alignGBL2 497
-#jobsub -c config.cfg -csv runlist.csv -g alignGBL3 497
+jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 497
+jobsub -c config.cfg -csv runlist.csv -g clustering 497
+jobsub -c config.cfg -csv runlist.csv -g hitmaker 497
+jobsub -c config.cfg -csv runlist.csv -g alignGBL1 497
+jobsub -c config.cfg -csv runlist.csv -g alignGBL2 497
+jobsub -c config.cfg -csv runlist.csv -g alignGBL3 497
 jobsub -c config.cfg -csv runlist.csv -g fitGBL 497
 
 #jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 498
@@ -12,7 +12,7 @@ jobsub -c config.cfg -csv runlist.csv -g fitGBL 497
 #jobsub -c config.cfg -csv runlist.csv -g alignGBL1 498
 #jobsub -c config.cfg -csv runlist.csv -g alignGBL2 498
 #jobsub -c config.cfg -csv runlist.csv -g alignGBL3 498
-jobsub -c config.cfg -csv runlist.csv -g fitGBL 498
+#jobsub -c config.cfg -csv runlist.csv -g fitGBL 498
 
 #jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 499
 #jobsub -c config.cfg -csv runlist.csv -g clustering 499
@@ -20,4 +20,4 @@ jobsub -c config.cfg -csv runlist.csv -g fitGBL 498
 #jobsub -c config.cfg -csv runlist.csv -g alignGBL1 499
 #jobsub -c config.cfg -csv runlist.csv -g alignGBL2 499
 #jobsub -c config.cfg -csv runlist.csv -g alignGBL3 499
-jobsub -c config.cfg -csv runlist.csv -g fitGBL 499
+#jobsub -c config.cfg -csv runlist.csv -g fitGBL 499
